ddfafdd16e8acdc27942cd30d98de9ab439a34d9
shilkon <sh1lkon@yandex.ru>
2023-04-08 17:56:29 +0300

    Added basic logging

--- src/main/java/com/hulk/magnit_phonenumber_database_service/jwt/ApplicationConfig.java
+++ src/main/java/com/hulk/magnit_phonenumber_database_service/jwt/ApplicationConfig.java
@@ -3,6 +3,8 @@ package com.hulk.magnit_phonenumber_database_service.jwt;
 
 import com.hulk.magnit_phonenumber_database_service.service.EmployeeServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,12 +20,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class ApplicationConfig {
     private final EmployeeServiceImpl service;
+    private static final Logger log = LoggerFactory.getLogger(ApplicationConfig.class);
 
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> service
                 .findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> {
+                    var e = new UsernameNotFoundException("User not found");
+                    log.warn(e.getMessage() + ", username: " + username);
+                    return e;
+                });
     }
 
     @Bean
@@ -32,6 +39,7 @@ public class ApplicationConfig {
 
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
+        log.info("Authentication provider has been set");
 
         return authProvider;
     }
--- src/main/java/com/hulk/magnit_phonenumber_database_service/service/EmployeeServiceImpl.java
+++ src/main/java/com/hulk/magnit_phonenumber_database_service/service/EmployeeServiceImpl.java
@@ -2,6 +2,8 @@ package com.hulk.magnit_phonenumber_database_service.service;
 
 import com.hulk.magnit_phonenumber_database_service.dao.EmployeeRepository;
 import com.hulk.magnit_phonenumber_database_service.entity.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -14,35 +16,51 @@ import java.util.UUID;
 public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
+    private static final Logger log = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     @Override
     public void saveEmployee(Employee employee) {
+        log.info("Saving employee with username: " + employee.getUsername());
         employeeRepository.save(employee);
     }
 
     @Override
     public List<Employee> getAllEmployees() {
+        log.info("Getting all employees");
         return employeeRepository.findAll();
     }
 
     @Override
     public Employee getEmployee(UUID id) {
+        log.info("Getting employee with id: " + id);
         return employeeRepository.findById(id)
-                .orElseThrow(() -> new UsernameNotFoundException("There is no user with this id"));
+                .orElseThrow(() -> {
+                    var e = new UsernameNotFoundException("There is no employee with this id");
+                    log.warn("Failed to get employee with id: " + id);
+                    return e;
+                });
     }
 
     @Override
     public void deleteEmployee(UUID id) {
+        log.info("Deleting employee with id: " + id);
         employeeRepository.deleteById(id);
     }
 
     @Override
     public Optional<Employee> findByEmail(String email) {
+        log.debug("Finding employee with email: " + email);
         return employeeRepository.findByEmail(email);
     }
 
     @Override
     public boolean existsUserByEmail(String email) {
+        boolean doesUserExists = employeeRepository.existsUserByEmail(email);
+        if (doesUserExists) {
+            log.debug("User exists with email: " + email);
+        } else {
+            log.debug("User does not exist with email: " + email);
+        }
         return employeeRepository.existsUserByEmail(email);
     }
 }
