88c80ff02263a3afa2407216a8b09c9988af465d
37hulk37 <60547863+37hulk37@users.noreply.github.com>
2023-04-23 23:27:07 +0300

    Implemented DTO pattern

--- src/main/java/com/hulk/magnit_phonenumber_database_service/controller/EmployeeController.java
+++ src/main/java/com/hulk/magnit_phonenumber_database_service/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.hulk.magnit_phonenumber_database_service.controller;
 
 import com.hulk.magnit_phonenumber_database_service.entity.Employee;
+import com.hulk.magnit_phonenumber_database_service.entity.EmployeeDTO;
+import com.hulk.magnit_phonenumber_database_service.entity.EmployeeDTOMapper;
+import com.hulk.magnit_phonenumber_database_service.entity.EmployeeMapper;
 import com.hulk.magnit_phonenumber_database_service.service.EmployeeService;
 import com.hulk.magnit_phonenumber_database_service.service.EmployeeSort;
 import jakarta.validation.constraints.Max;
@@ -8,50 +11,45 @@ import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("/home")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
-
-    @GetMapping()
-    public String login(Model model) {
-        return "login";
-    }
+    @Autowired
+    private EmployeeDTOMapper employeeDTOMapper;
 
     @PostMapping("/employees")
-    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody Employee employee) {
         employeeService.saveEmployee(employee);
 
-        return ResponseEntity.ok(employee);
+        return ResponseEntity.ok(employeeDTOMapper.apply(employee));
     }
 
     @PutMapping("/employees")
-    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+    public ResponseEntity<EmployeeDTO> updateEmployee(@RequestBody Employee employee) {
         employeeService.saveEmployee(employee);
 
-        return ResponseEntity.ok(employee);
+        return ResponseEntity.ok(employeeDTOMapper.apply(employee));
     }
 
     @GetMapping("/employees/{id}")
-    public ResponseEntity<Employee> getEmployee(@PathVariable UUID id) {
+    public ResponseEntity<EmployeeDTO> getEmployee(@PathVariable UUID id) {
         return ResponseEntity.ok(employeeService.getEmployee(id));
     }
 
     @GetMapping("/employees/all")
-    public ResponseEntity<List<Employee>> getAllEmployees() {
+    public ResponseEntity<List<EmployeeDTO>> getAllEmployees() {
         return ResponseEntity.ok(employeeService.getAllEmployees());
     }
 
     @GetMapping("/employees")
-    public ResponseEntity<Page<Employee>> getEmployees(
+    public ResponseEntity<Page<EmployeeDTO>> getEmployees(
             @RequestParam(value = "offset", defaultValue = "0") @Min(0) int offset,
             @RequestParam(value = "limit", defaultValue = "20") @Min(1) @Max(40) int limit,
             @RequestParam(value = "sort", defaultValue = "ID_ASC") EmployeeSort sort
