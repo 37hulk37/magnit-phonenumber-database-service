d7563f9a2057080505720452fbc8eda8bf106fad
anvax <andreykhudobin23@gmail.com>
2023-05-11 22:32:54 +0300

    AuthErrors

---
+++ src/main/java/com/hulk/magnit_phonenumber_database_service/exception/EmpSurnameException.java
@@ -0,0 +1,8 @@
+package com.hulk.magnit_phonenumber_database_service.exception;
+
+public class EmpSurnameException extends ApiException {
+    public EmpSurnameException(String message) {
+        super(message);
+    }
+}
+//
